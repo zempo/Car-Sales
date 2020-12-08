91496d6852723289083e9f3063aa749ad34de2a8
zempo <zelenkosolomon@gmail.com>
2020-12-08 14:48:59 -0800

    add redux hooks to actions

--- src/actions/carActions.js
+++ src/actions/carActions.js
@@ -1,3 +1,18 @@
 export const ADD_FEATURE = "ADD_FEATURE";
 export const EDIT_FEATURE = "EDIT_FEATURE";
 export const DELETE_FEATURE = "DELETE_FEATURE";
+
+// redux hooks
+export const addFeature = (feature) => {
+  return {
+    type: ADD_FEATURE,
+    payload: feature,
+  };
+};
+
+export const removeFeature = (feature) => {
+  return {
+    type: REMOVE_FEATURE,
+    payload: feature,
+  };
+};
